b22b78af66e919d7bd7078db3620f44384d1106f
Stanislav <90218189+UtheronS@users.noreply.github.com>
2023-09-03 17:16:04 +0300

    Added source files

---
+++ homework_7.py
@@ -0,0 +1,72 @@
+import os
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+""" 1) Напишите функцию группового переименования файлов. Она должна:
+1. принимать параметр желаемое конечное имя файлов. При переименовании в конце имени добавляется порядковый номер.
+2. принимать параметр количество цифр в порядковом номере.
+3. принимать параметр расширение исходного файла. Переименование должно работать только для этих файлов внутри каталога.
+4. принимать параметр расширение конечного файла.
+5. принимать диапазон сохраняемого оригинального имени.
+Например, для диапазона [3, 6] берутся буквы с 3 по 6 из исходного имени файла.
+К ним прибавляется желаемое конечное имя, если оно передано. Далее счётчик файлов и расширение."""
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+
+def group_renaming(directory, wanted_name, num_digits, initial_extension, final_extension, name_range=None):
+    if not os.path.exists(directory):
+        print('Такого каталога не существует')
+        return
+
+    for filename in os.listdir(directory):
+        if filename.endswith(initial_extension):
+            base_name = os.path.splitext(filename)[0]
+
+        if name_range:
+            start, end = name_range
+            if start < 0:
+                start = 0
+            if end > len(base_name):
+                end = len(base_name)
+            base_name = base_name[start:end]
+
+        count = 1
+        while os.path.exists(os.path.join(directory, f"{wanted_name}{count:0{num_digits}d}.{final_extension}")):
+            count += 1
+
+        new_filename = f"{wanted_name}{count:0{num_digits}d}.{final_extension}"
+
+        os.rename(os.path.join(directory, filename), os.path.join(directory, new_filename))
+        print(f"Переименован {filename} в {new_filename}")
+
+
+# group_renaming('D:\\PycharmProjects\\rename_files', 'result_name', 2, 'py', 'ipynb', (3, 6))
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+""" 2) Соберите из созданных на уроке и в рамках домашнего задания функций пакет для работы с файлами."""
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+folder_path = 'D:\\PycharmProjects\\rename_files'
+
+if not os.path.exists(folder_path):
+    os.mkdir(folder_path)
+
+file_name = '__init__.py'
+file_path = os.path.join(folder_path, file_name)
+
+code = """
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+"""
+
+with open(file_path, 'w', encoding='utf-8') as f:
+    f.write(code)
+# ---------------------------------------------------------------------------------------------------------------------
